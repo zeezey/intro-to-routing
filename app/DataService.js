84589011d80a3be36c34e5c7e9d24b45a784c208
Jeremy R <jeremy@devmounta.in>
2015-07-27 09:34:23 -0600

    first

---
+++ app/DataService.js
@@ -0,0 +1,11 @@
+app.service('DataService', function(){
+	return {
+		data: [
+			{ name: 'John', description: 'John loves cake'},
+			{ name: 'Sara', description: 'Sara loves pie'},
+			{ name: 'Joe', description: 'Joe loves cookies'},
+			{ name: 'Michelle', description: 'Michelle loves brownies'},
+			{ name: 'Dave', description: 'Dave loves Dave'}
+		]
+	}
+});
